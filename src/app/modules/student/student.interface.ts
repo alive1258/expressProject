0e286a3f8de0cca4e703ac3a0dc1747be7309b2c
alive1258 <javashazib@gmail.com>
2023-11-17 17:16:31 +0600

    student schema create done

---
+++ src/app/modules/student/student.interface.ts
@@ -0,0 +1,37 @@
+// 1. Create an interface representing a document in MongoDB.
+export type Username = {
+  firstName: string;
+  middleName: string;
+  lastName: string;
+};
+export type Guardian = {
+  fatherName: string;
+  fatherOccupation: string;
+  fatherContact: string;
+  motherName: string;
+  motherOccupation: string;
+  motherContact: string;
+};
+export type LocalGuardian = {
+  name: string;
+  occupation: string;
+  contactNo: string;
+  address: string;
+};
+
+export type Student = {
+  id: string;
+  name: Username;
+  gender: 'male' | 'female';
+  dateOfBirth: string;
+  email: string;
+  contactNo: string;
+  emergencyContactNo: string;
+  bloodGroup?: 'A+' | 'A-' | 'B+' | 'B-' | 'AB+' | 'AB-' | 'O+' | 'O-';
+  presentAddress: string;
+  permanentAddress: string;
+  guardian: Guardian;
+  localGuardian: LocalGuardian;
+  profileImg?: string;
+  isActive: 'active' | 'blocked';
+};
